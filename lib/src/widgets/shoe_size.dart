d258a44a5e94ada9801c36e88bffdbacbb019d83
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-27 18:53:48 -0700

    shoe shadow

--- lib/src/widgets/shoe_size.dart
+++ lib/src/widgets/shoe_size.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ShoeSizePreview extends StatelessWidget {
   const ShoeSizePreview({super.key});
@@ -14,15 +15,15 @@ class ShoeSizePreview extends StatelessWidget {
             color: const Color(0xffFFCF53),
             borderRadius: BorderRadius.circular(50)),
         child: const Column(
-          children: [_ShadowShoe()],
+          children: [_ShoeBox()],
         ),
       ),
     );
   }
 }
 
-class _ShadowShoe extends StatelessWidget {
-  const _ShadowShoe();
+class _ShoeBox extends StatelessWidget {
+  const _ShoeBox();
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +31,27 @@ class _ShadowShoe extends StatelessWidget {
       padding: const EdgeInsets.all(50.0),
       child: Stack(
         children: [
-          Image(image: AssetImage('assets/imgs/azul.png')),
+          Positioned(bottom: 20, right: 0, child: _ShoeShadow()),
+          const Image(image: AssetImage('assets/imgs/azul.png')),
         ],
+      ),
+    );
+  }
+}
+
+class _ShoeShadow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        width: 230,
+        height: 140,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: const [
+              BoxShadow(color: Color(0xffEAA14E), blurRadius: 40)
+            ]),
       ),
     );
   }
